2baf770de6f7f98ee4ecea6d00e847f3a021689b
Akash S <akashspalloor@gmail.com>
2025-10-02 16:21:17 +0530

    responsive updated

--- src/components/ui/StickyScroll.jsx
+++ src/components/ui/StickyScroll.jsx
@@ -19,12 +19,12 @@ const StickyScrollLayout = () => {
       </section>
 
       {/* Second*/}
-      <section className="text-white h-screen w-full bg-slate-950 grid place-content-center sticky top-0">
+      <section className="text-white h-screen w-full bg-slate-950 grid place-content-center sticky top-0 hidden md:block">
         <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:54px_54px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]" />
       </section>
 
       {/* Second section contend*/}
-      <section className="text-white w-full bg-black">
+      <section className="text-white w-full bg-black hidden md:block">
         <div className="grid grid-cols-2">
           <div className="sticky top-0 h-screen flex items-center justify-center">
             <h1 className="2xl:text-7xl text-5xl px-8 font-semibold text-center tracking-tight leading-[120%]">
