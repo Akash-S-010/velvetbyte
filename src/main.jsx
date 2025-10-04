f27b3fad9e4b14475e449e6222d23591b7c023b9
Akash S <akashspalloor@gmail.com>
2025-10-04 12:22:19 +0530

    updated smooth scroll

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { ReactLenis } from "lenis/react"; 
+import { ReactLenis } from "lenis/react";
 
 import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ReactLenis root options={{ lerp: 0.08, smoothWheel: true, duration: 1.2 }}>
+    <ReactLenis
+      root
+      options={{
+        lerp: 0.06, // very smooth, slower interpolation
+        smoothWheel: true, // makes mouse and trackpad smooth
+        duration: 1.6, // scrolls take longer to settle
+        smoothTouch: true, // optional: smooth scrolling on touch devices
+      }}
+    >
       <BrowserRouter>
         <App />
       </BrowserRouter>
