148fb7857cb086712f0f8c6d285294d2f0a07147
anshifaanshi <anshifajamsher@gmail.com>
2025-10-06 20:09:22 +0300

    Resolved merge conflict in Services.jsx

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import StickySection from "../components/ui/StickySection";
 
@@ -13,7 +12,7 @@ const Services = () => {
         "https://i.pinimg.com/736x/e1/62/9c/e1629c2ef715f4e8be3a349f461dd6b9.jpg",
       ],
       description:
-        "Boost your business with custom websites, WordPress, Shopify, and eCommerce solutions designed to captivate and convert. Engage users, streamline shopping, and skyrocket sales with stunning, user-friendly designs tailored to your brand. Transform clicks into loyal customers with our high-impact, conversion-driven digital storefronts. Launch your success today!success.",
+        "Boost your business with custom websites, WordPress, Shopify, and eCommerce solutions designed to captivate and convert. Engage users, streamline shopping, and skyrocket sales with stunning, user-friendly designs tailored to your brand. Transform clicks into loyal customers with our high-impact, conversion-driven digital storefronts. Launch your success today!",
       technologies: [
         "WordPress CMS",
         "Shopify & WooCommerce",
@@ -32,7 +31,7 @@ const Services = () => {
         "https://i.pinimg.com/736x/e1/62/9c/e1629c2ef715f4e8be3a349f461dd6b9.jpg",
       ],
       description:
-        "Discover our cross-platform apps, designed with intuitive, user-friendly interfaces to captivate and engage customers. Seamlessly connect across devices, delivering immersive experiences that boost interaction, loyalty, and satisfaction. Elevate your brand with dynamic, responsive apps crafted to keep users hooked and drive meaningful engagement in every interaction.udience.",
+        "Discover our cross-platform apps, designed with intuitive, user-friendly interfaces to captivate and engage customers. Seamlessly connect across devices, delivering immersive experiences that boost interaction, loyalty, and satisfaction. Elevate your brand with dynamic, responsive apps crafted to keep users hooked and drive meaningful engagement.",
       technologies: [
         "Fully Responsive Designs",
         "Animated & Interactive",
@@ -50,7 +49,7 @@ const Services = () => {
         "https://i.pinimg.com/736x/e1/62/9c/e1629c2ef715f4e8be3a349f461dd6b9.jpg",
       ],
       description:
-        "Boost your business with powerful SEO, dynamic social media, targeted PPC, impactful branding, and high-ROI performance marketing. Drive massive traffic, engage your audience, and skyrocket revenue with strategies that deliver results. Connect with customers, amplify your brand, and dominate your market today!usiness.",
+        "Boost your business with powerful SEO, dynamic social media, targeted PPC, impactful branding, and high-ROI performance marketing. Drive massive traffic, engage your audience, and skyrocket revenue with strategies that deliver results. Connect with customers, amplify your brand, and dominate your market today!",
       technologies: [
         "SEO",
         "Social Media Marketing",
@@ -68,7 +67,7 @@ const Services = () => {
         "https://i.pinimg.com/736x/e1/62/9c/e1629c2ef715f4e8be3a349f461dd6b9.jpg",
       ],
       description:
-        "Crafting intuitive, user-centric experiences and stunning brand visuals that captivate and engage audiences. Our designs blend creativity and functionality, ensuring seamless interactions and unforgettable impressions. Elevate your brand with bold, innovative aesthetics tailored to resonate deeply and drive meaningful connections in every digital touchpoint.tations.",
+        "Crafting intuitive, user-centric experiences and stunning brand visuals that captivate and engage audiences. Our designs blend creativity and functionality, ensuring seamless interactions and unforgettable impressions. Elevate your brand with bold, innovative aesthetics tailored to resonate deeply and drive meaningful connections.",
       technologies: [
         "Creative Ads",
         "Poster Designing",
@@ -85,7 +84,7 @@ const Services = () => {
         "https://i.pinimg.com/736x/e1/62/9c/e1629c2ef715f4e8be3a349f461dd6b9.jpg",
       ],
       description:
-        "Craft a bold brand identity with striking logos, vibrant brochures, and a dynamic digital presence. Establish a unique voice that captivates and connects, driving engagement and loyalty. Elevate your brand with strategic storytelling and visuals that resonate, leaving a lasting impression on your audience.tations.",
+        "Craft a bold brand identity with striking logos, vibrant brochures, and a dynamic digital presence. Establish a unique voice that captivates and connects, driving engagement and loyalty. Elevate your brand with strategic storytelling and visuals that resonate, leaving a lasting impression on your audience.",
       technologies: [
         "Logos",
         "Brochures",
@@ -102,35 +101,22 @@ const Services = () => {
         "https://i.pinimg.com/736x/e1/62/9c/e1629c2ef715f4e8be3a349f461dd6b9.jpg",
       ],
       description:
-        "Transform your business with our custom IT solutions! We craft tailored software and cutting-edge automation tools to supercharge efficiency, streamline operations, and drive growth. Unlock your potential with innovative, user-focused technology designed to fit your unique needs. Partner with us to revolutionize your workflow and achieve unparalleled success!tations.",
+        "Transform your business with our custom IT solutions! We craft tailored software and cutting-edge automation tools to supercharge efficiency, streamline operations, and drive growth. Unlock your potential with innovative, user-focused technology designed to fit your unique needs. Partner with us to revolutionize your workflow and achieve unparalleled success!",
       technologies: [
-        "Ai",
+        "AI",
         "IoT",
         "Automation",
         "Custom Software",
-        "Cloud Computing",],
+        "Cloud Computing",
+      ],
     },
   ];
 
   return (
     <div className="min-h-screen">
-      <div>
-        
-      </div>
       <StickySection services={services} />
     </div>
   );
 };
 
 export default Services;
-=======
-import React from 'react'
-
-function Services() {
-  return (
-    <div>Services</div>
-  )
-}
-
-export default Services
->>>>>>> 9be1767 (about us)
