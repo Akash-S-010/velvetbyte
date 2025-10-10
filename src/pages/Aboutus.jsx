534818bd56627f3421b256079277dce7fabe0c24
Akash S <akashspalloor@gmail.com>
2025-10-10 18:37:15 +0530

    fixed issues

--- src/pages/Aboutus.jsx
+++ src/pages/Aboutus.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import CreativeWebHero from '../components/aboutsections/CreateWebHero'
-import StatsSection from '../components/aboutsections/StatsSection'
-import AgencyHighlights from '../components/aboutsections/AgencyHighlights'
-import CareerOpportunities from '../components/aboutsections/CareerOpportunities'
-import SpeakUsBtn from '../components/ui/SpeakUsBtn'
+import React from "react";
+import CreativeWebHero from "../components/aboutsections/CreateWebHero";
+import StatsSection from "../components/aboutsections/StatsSection";
+import AgencyHighlights from "../components/aboutsections/AgencyHighlights";
+import CareerOpportunities from "../components/aboutsections/CareerOpportunities";
+import SpeakUsBtn from "../components/ui/SpeakUsBtn";
 function AboutUs() {
   return (
     <div>
-        <CreativeWebHero/>
-        <StatsSection/>
-        <AgencyHighlights/>
-        <CareerOpportunities/>
-        <SpeakUsBtn>Speak to Us</SpeakUsBtn>
+      <CreativeWebHero />
+      <StatsSection />
+      <AgencyHighlights />
+      <CareerOpportunities />
+      <SpeakUsBtn>Speak to Us</SpeakUsBtn>
     </div>
-  )
+  );
 }
 
-export default AboutUs
+export default AboutUs;
