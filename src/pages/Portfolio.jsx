b668216fecd35ee7c2be7d83e4ff2496b09632f2
Akash S <akashspalloor@gmail.com>
2025-10-10 17:16:07 +0530

    updated responsive issues

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -2,14 +2,12 @@ import React from 'react'
 import WebsiteCostChart from '../components/portfoliosections/WebsiteCostChart'
 import CostSection from '../components/portfoliosections/CostSection'
 import ComparisonTable from '../components/portfoliosections/ComparisonTable'
-import SpeakUsBtn from '../components/ui/SpeakUsBtn'
 function Portfolio() {
   return (
     <div>
         <WebsiteCostChart/>
         <CostSection/>
         <ComparisonTable/>
-        <SpeakUsBtn/>
     </div>
   )
 }
