1b7cac50463615f6c0723b485d444545feb83171
Akash S <akashspalloor@gmail.com>
2025-10-11 14:27:30 +0530

    added AboutHero

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import CreativeWebHero from "../components/aboutSections/CreativeWebHero";
+import AboutHero from "../components/aboutSections/AboutHero";
 import AgencyHighlights from "../components/aboutSections/AgencyHighlights";
 import CareerOpportunities from "../components/aboutSections/CareerOpportunities";
 import StatsSection from "../components/aboutSections/StatsSection";
@@ -7,7 +7,7 @@ import ProfileCard from "../components/aboutSections/ProfileCard";
 function AboutUs() {
   return (
     <div>
-      <CreativeWebHero />
+    <AboutHero />
       <StatsSection />
       <div className="mt-10">
         <h1 className="text-xl md:text-3xl lg:text-5xl text-center font-semibold mb-10">Meet Our <span className="text-primary">Team</span></h1>
