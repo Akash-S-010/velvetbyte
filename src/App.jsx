ee18e72688c6f2d086cd4f822dbdb62422a058cc
Akash S <akashspalloor@gmail.com>
2025-10-11 14:32:59 +0530

    fixing import

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import ScrollProgressBar from "./components/ui/ScrollProgressBar";
-import About from "./pages/About";
 import Services from "./pages/Services";
 import ContactUs from "./pages/ContactUs";
 import Portfolio from "./pages/Portfolio";
