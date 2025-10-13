2dc8a287bf405b7a101941f2bf69d18607daf0ab
Akash S <akashspalloor@gmail.com>
2025-10-13 17:34:12 +0530

    added client section

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -4,10 +4,12 @@ import AgencyHighlights from "../components/aboutSections/AgencyHighlights";
 import CareerOpportunities from "../components/aboutSections/CareerOpportunities";
 import StatsSection from "../components/aboutSections/StatsSection";
 import ProfileCard from "../components/aboutSections/ProfileCard";
+import ClientSection from "../components/ui/ClientSection";
 function AboutPage() {
   return (
     <div>
     <AboutHero />
+      <ClientSection />
       <StatsSection />
       <div className="mt-10">
         <h1 className="text-xl md:text-3xl lg:text-5xl text-center font-semibold mb-10">Meet Our <span className="text-primary">Team</span></h1>
