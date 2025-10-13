055ff7b6a61e1cc0aedf8ca9e2c777adaf4905f4
Akash S <akashspalloor@gmail.com>
2025-10-13 15:59:22 +0530

    added Works sample on portfolio

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import WebsiteCostChart from '../components/portfoliosections/WebsiteCostChart'
 import CostSection from '../components/portfoliosections/CostSection'
 import ComparisonTable from '../components/portfoliosections/ComparisonTable'
+import OurWorks from '../components/portfoliosections/OurWorks'
 function Portfolio() {
   return (
     <div>
         <WebsiteCostChart/>
+          <OurWorks/>
         <CostSection/>
         <ComparisonTable/>
     </div>
