0d9dbdbe903446bc25caa4d4ea1b5fea26ff2b1c
Akash S <akashspalloor@gmail.com>
2025-10-17 11:19:47 +0530

    updated logo slider

--- src/components/ui/ClientSection.jsx
+++ src/components/ui/ClientSection.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import './ClientSection.css';
+import "./ClientSection.css";
 
 const clients = [
   { name: "Fullmoon", logo: "/images/client_logos/Fullmoon.jpg" },
   { name: "Selorae", logo: "/images/client_logos/Selorae.png" },
   { name: "Blacmelo", logo: "/images/client_logos/Blacmelo.png" },
-  { name: "Vikriti", logo: "/images/client_logos/Vikriti.avif" },
+  { name: "Vikriti", logo: "/images/client_logos/Vikriti.png" },
+  { name: "PureMed", logo: "/images/client_logos/Puremed.png" },
+  { name: "Soda", logo: "/images/client_logos/SODA.png" },
 ];
 
 // Duplicate logos for seamless infinite loop
@@ -14,11 +16,9 @@ const repeatedClients = [...clients, ...clients];
 export default function ClientsSection() {
   return (
     <section className="bg-white mb-20 relative overflow-hidden py-4 md:py-10">
-      <div className="max-w-8xl mx-auto px-6">
+      <div className="max-w-7xl mx-auto px-6">
         {/* Heading */}
-        <h2
-          className="text-2xl md:text-5xl font-semibold text-gray-800 mb-4 md:mb-10 text-center"
-        >
+        <h2 className="text-2xl md:text-5xl font-semibold text-gray-800 mb-6 md:mb-10 text-center">
           Trusted By Leading
           <br />
           <span className="text-primary">Companies</span>
@@ -26,18 +26,13 @@ export default function ClientsSection() {
 
         {/* Sliding Logo Container */}
         <div className="logos-container">
-          
-          {/* Logo Track */}
           <div className="logo-track">
             {repeatedClients.map((client, index) => (
-              <div
-                key={`${client.name}-${index}`}
-                className="logo-item"
-              >
+              <div key={`${client.name}-${index}`} className="logo-item cursor-pointer">
                 <img
                   src={client.logo}
                   alt={client.name}
-                  className="max-h-full max-w-full object-contain grayscale hover:grayscale-0 transition-all duration-300"
+                  className="transition-all duration-500"
                   loading="lazy"
                 />
               </div>
